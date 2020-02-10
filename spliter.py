637c7f9ce8d94af5ccea10bd0efac88917cd6c46
Hacker1337 <amirfvb@gmail.com>
2020-02-10 23:15:26 +0300

    Данные

--- spliter.py
+++ spliter.py
@@ -1,9 +1,14 @@
-out = open("Данные1таблица.txt", 'w')
-src = open("Данные1.txt", 'r')
+out = open("Данные2таблица.txt", 'w')
+src = open("Данные2.txt", 'r')
+i = 0
 while (src.readable()):
     a = src.readline().strip().split()
     b = src.readline().strip().split()
     if (len(a) == 0 or len(b) == 0):
         break
+    if (int(a[-1]) < i):
+        out.write(str(int(a[-1]) + 45323) + "\t" + b[-1] + "\n")
+    else:
+        out.write(a[-1] + "\t" + b[-1] + "\n")
+    i+=1
 
-    out.write(a[-1] + "\t" + b[-1] + "\n")
